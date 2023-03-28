dcd729ed9ac325ee668039387ca90a0c838df460
Elcio Yoshiaki Sato <elciosato@gmail.com>
2023-03-28 14:22:39 +0200

    Add mongodb for github actions

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const MONGO_URL = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.dhfrtyk.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
-
+const MONGO_URL = process.env.MONGO_URL;
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection open!");
 });
